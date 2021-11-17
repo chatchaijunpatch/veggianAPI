e4d611db892ee1d061f554168dfd3c9e43da7924
chatchai junpatch <chatchai.ju@ku.th>
2021-11-17 18:21:27 +0700

    update DB

--- src/main/java/th/ac/ku/restaurant/repository/OrderRepository.java
+++ src/main/java/th/ac/ku/restaurant/repository/OrderRepository.java
@@ -2,7 +2,7 @@ package th.ac.ku.restaurant.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import th.ac.ku.restaurant.model.User;
+//import th.ac.ku.restaurant.model.User;
 import th.ac.ku.restaurant.model.VegOrder;
 
 import java.util.List;
